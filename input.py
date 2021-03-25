08a7b33ddfb8a2b13803f387e05fef009f70350f
Jack2ee-dev <Jack2ee.dev@gmail.com>
2021-03-25 15:58:45 +0900

    fix: fix unboundLocalError for getting input

--- input.py
+++ input.py
@@ -1,8 +1,7 @@
 def get_input():
     print("숫자를 입력하세요: ")
     try:
-        input = input()
-        number = int(input)
+        number = int(input())
         return number
     except ValueError:
         get_input()
