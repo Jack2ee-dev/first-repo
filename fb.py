cda60c3b7c741ea33a916d2bb46248fd47f25181
Jack2ee-dev <Jack2ee.dev@gmail.com>
2021-03-25 15:59:21 +0900

    refactor: Eliminate unnecessary variable start

--- fb.py
+++ fb.py
@@ -1,20 +1,24 @@
 from input import get_input
 
+
 def do_fizzbuzz():
-    user_input = get_input()
-    start = 1
-    end = user_input
-    curr = 1
-    while curr < end:
-        if curr % 3 == 0:
-            print("fizz")
-        elif curr % 5 == 0:
-            print("buzz")
-        elif curr % 15 == 0:
-            print("fizzbuzz")
-        else:
-            print(curr)
-        curr += 1
+    try:
+        user_input = get_input()
+        end = user_input
+        curr = 1
+        while curr < end:
+            if curr % 3 == 0:
+                print("fizz")
+            elif curr % 5 == 0:
+                print("buzz")
+            elif curr % 15 == 0:
+                print("fizzbuzz")
+            else:
+                print(curr)
+            curr += 1
+    except ValueError:
+        return
+
 
 if __name__ == "__main__":
     do_fizzbuzz()
